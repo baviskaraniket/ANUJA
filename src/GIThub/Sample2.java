18f03ccff72d63c6c359e8700f2b2e1e03e5b7a0
dell <dell@ANIKET>
2022-07-25 23:58:12 +0530

    ANUJA I MADE U PROG BY USING WHILE LOOP

---
+++ src/GIThub/Sample2.java
@@ -0,0 +1,16 @@
+package GIThub;
+
+public class Sample2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int i=1;
+		while(i<10)
+		{
+			System.out.println(i);
+			i++;
+		}
+	}
+
+}
