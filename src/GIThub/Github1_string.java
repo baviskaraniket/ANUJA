a9b9291c7ddd5798067f36fe8d4b8fca0b6c3b6f
dell <dell@ANIKET>
2022-07-25 22:16:21 +0530

    iam sharing first time by using github

---
+++ src/GIThub/Github1_string.java
@@ -0,0 +1,13 @@
+package GIThub;
+
+public class Github1_string {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String Github="I AM READY WITH GITHUB FOR PRACTICE";
+		System.out.println(Github);
+
+	}
+
+}
